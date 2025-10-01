d32425144d9095c6cf7b641fe8be5c186ac6cbcc
Lucas <lucashenriquecardosoprado@gmail.com>
2025-10-01 15:14:07 -0300

    4 - Criando a lógica da Controller Usuario

--- ProjetoLoja/Controllers/UsuarioController.cs
+++ ProjetoLoja/Controllers/UsuarioController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// IMPORTANDO PACOTES PARA UTILIZAR NO PROJETO
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLoja.Repositorio;
 
+// DEFINE O NOME E ONDE A CLASSE ESTÁ LOCALIZADA, NAMESPACE AJUDA A ORGANIZAR O CÓDIGO E EVITAR CONFLITOS DE NOMES
 namespace ProjetoLoja.Controllers
 {
+    // CLASSE USUARIO CONTROLLER QUE ESTÁ HERDANDO DA CLASSE CONTROLLER
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        // DECLARA UMA VARIÁVEL PRIVADA, SOMENTE LEITURA DO TIPO UsuarioRepositorio CHAMADA (INSTANCIAR) _usuarioRepositorio
+        // UsuarioRepositorio É UMA CLASSE RESPONSÁVEL POR INTERAGIR COM A CAMADA DE DADOS E GERENCIAR AS INFORMAÇÕES DO USUARIO
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        //CONSTRUTOR QUE RECEBE A INSTÂNCIA UsuarioRepositorio COM PARÂMETROS (INJEÇÃO DE DEPENDÊNCIA)
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
         {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        // INTERFACE É UMA REPRESENTAÇÃO DO RESULTADO (TELA)
+        public IActionResult Login()
+        {
+            // RETORNA A PÁGINA LOGIN
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(String email, String senha)
+        {
+            var Usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (Usuario != null && Usuario.senha != senha)
+            {
+                return RedirectToAction("Index", "Cliente");
+            }
+            ModelState.AddModelError("", "Email / Senha inválidos");
+
+            // RETORNA A PÁGINA LOGIN
             return View();
         }
     }
