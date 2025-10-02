f11cd772ec695d528ba191afd5e13bf8840a416e
Lucas <lucashenriquecardosoprado@gmail.com>
2025-10-02 17:39:36 -0300

    7 - Criando a lógica do UsuárioRepositório

--- ProjetoLoja/Repositorio/UsuarioRepositorio.cs
+++ ProjetoLoja/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,15 @@
-﻿namespace ProjetoLoja.Repositorio
+﻿using MySql.Data.MySqlClient;
+using ProjetoLoja.Models;
+namespace ProjetoLoja.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public Usuario ObterUsuario(string email)
+        {
+
+        }
     }
 }
