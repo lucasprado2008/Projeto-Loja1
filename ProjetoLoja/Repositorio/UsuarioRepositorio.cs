b904775bc7f3a1ae08015e9bef8784414959f81c
Lucas <lucashenriquecardosoprado@gmail.com>
2025-10-09 17:04:27 -0300

    11.5 - Corrigindo o banco de dados

--- ProjetoLoja/Repositorio/UsuarioRepositorio.cs
+++ ProjetoLoja/Repositorio/UsuarioRepositorio.cs
@@ -58,8 +58,6 @@ namespace ProjetoLoja.Repositorio
                         {
                             // Lê o valor da coluna "Id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'Id' do objeto 'usuario'.
                             Id = Convert.ToInt32(dr["id"]),
-                            // Lê o valor da coluna "Nome" da linha atual do resultado, converte para string e atribui à propriedade 'Nome' do objeto 'usuario'.
-                            Nome = dr["nome"].ToString(),
                             // Lê o valor da coluna "Email" da linha atual do resultado, converte para string e atribui à propriedade 'Email' do objeto 'usuario'.
                             Email = dr["email"].ToString(),
                             // Lê o valor da coluna "Senha" da linha atual do resultado, converte para string e atribui à propriedade 'Senha' do objeto 'usuario'.
